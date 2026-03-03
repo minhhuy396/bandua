0615ce06bb03386ab85b92cfcc3a1a032341b679
Nguyen Minh Huy <nguyen.huy2006@icloud.com>
2026-03-03 14:32:29 +0700

    add build.sh

--- shopweb/settings.py
+++ shopweb/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(7xlbkaj5bmwnuucq5x#pr&rmzu7oc2()@0&b9gh0-bng1%#zn'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['shopweb.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
